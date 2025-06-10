f857befc778ae7fb3fb1650981b9138d19c54041
Thien Tran <gau.nernst@yahoo.com.sg>
2025-06-10 22:38:32 +0800

    rename the redirected route from root

--- litellm/proxy/proxy_server.py
+++ litellm/proxy/proxy_server.py
@@ -8143,7 +8143,7 @@ async def get_litellm_model_cost_map():
 
 @router.get("/")
 async def home(request: Request):
-    return RedirectResponse(url="/fallback/login")
+    return RedirectResponse(url="/login")
 
 @router.get("/routes", dependencies=[Depends(user_api_key_auth)])
 async def get_routes():
