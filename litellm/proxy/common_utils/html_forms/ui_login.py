6827445bca44d79632dade5dc9e1377eb54f29bd
Daniel Ching <dancwq2@gmail.com>
2025-06-12 12:48:28 +0800

    Changed litellm header to menlo for login page

--- litellm/proxy/common_utils/html_forms/ui_login.py
+++ litellm/proxy/common_utils/html_forms/ui_login.py
@@ -7,7 +7,7 @@ html_form = f"""
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>LiteLLM Login</title>
+    <title>Menlo Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
         body {{
