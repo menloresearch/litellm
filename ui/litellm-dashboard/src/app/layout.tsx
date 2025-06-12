fa9877ac25c01120787e738c3740e206bca7ec0c
Daniel Ching <dancwq2@gmail.com>
2025-06-12 12:44:29 +0800

    Tab title change for dashboard

--- ui/litellm-dashboard/src/app/layout.tsx
+++ ui/litellm-dashboard/src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "LiteLLM Dashboard",
-  description: "LiteLLM Proxy Admin UI",
+  title: "Menlo Dashboard",
+  description: "Menlo Proxy Admin UI",
 };
 
 export default function RootLayout({
