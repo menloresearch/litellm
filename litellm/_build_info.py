e979e9f237fa9d419f55f5f72062859118c0266e
Thien Tran <gau.nernst@yahoo.com.sg>
2025-06-19 10:48:26 +0800

    use GIT_COMMIT env var

--- litellm/_build_info.py
+++ litellm/_build_info.py
@@ -1,1 +1,16 @@
-GIT_COMMIT = "UNKNOWN"
+import os
+import subprocess
+import functools
+
+
+@functools.lru_cache(1)
+def get_commit_hash():
+    # GIT_COMMIT env var is available for Docker build
+    if "GIT_COMMIT" in os.environ:
+        return os.environ["GIT_COMMIT"]
+
+    try:
+        # git is available for local development
+        return subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode().strip()
+    except Exception:
+        return "UNKNOWN"
